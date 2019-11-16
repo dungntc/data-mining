d8a3994384bafdb76de94da776bc8d712978243d
unknown <dungntc108@gmail.com>
2019-11-16 20:52:54 +0700

    add code

--- data_mining/labelling/urls.py
+++ data_mining/labelling/urls.py
@@ -1,13 +1,14 @@
 from django.conf.urls import url, include
 
-from .views import getAllLabels, home ,runCommand ,getOneLabel,update , runSelect3
+from .views import getAllLabels, home ,runCommand ,getOneLabel,update , run1,get1
 
 
 urlpatterns = [
     url(r'^api/get-all', getAllLabels, name='getAllLabels'),
+    url(r'^api/get1', get1, name='get1'),
     url(r'^api/get-one', getOneLabel, name='getOneLabel'),
     url(r'^api/update', update, name='update'),
     url(r'^api/run/$', runCommand, name='runCommand'),
-    url(r'^api/run1/$', runSelect3, name='runSelect3'),
+    url(r'^api/run1/$', run1, name='run1'),
     url(r'^$', home, name='home'),
 ]
--- data_mining/labelling/views.py
+++ data_mining/labelling/views.py
@@ -39,6 +39,20 @@ def getOneLabel(request):
         'status': True
     }, safe=False, json_dumps_params={'ensure_ascii': False}, status =200)
 
+@api_view(['GET'])
+def get1(request):
+
+    Lines = [41 ,77 ,133 ,149 ,293 ,367 ,380 ,557 ,625 ,988 ,1022 ,1025 ,1152 ,1169 ,1196 ,1249 ,1252 ,1260 ,1326 ,1405 ,1490 ,1506 ,1536 ,1546 ,1585 ,1590 ,1637 ,1699 ,1721 ,1758 ,1800 ,1815 ,1898 ,1917 ,2081 ,2131 ,2385 ,2395 ,2399 ,2403 ,2456 ,2646 ,2647 ,2747 ,2844 ,2907 ,2909 ,2933 ,3145 ,3218 ,3393 ,3412 ,3502 ,3559 ,3588 ,3614 ,3633 ,3663 ,3718 ,3755 ,3928 ,4142 ,4143 ,4161 ,4220 ,4226 ,4551 ,4564 ,4681 ,4740 ,4745 ,4819 ,5088 ,5300 ,5373 ,5425 ,5470 ,5487 ,5598 ,5712 ,5780 ,5885 ,6045 ,6051 ,6090 ,6105 ,6171 ,6220 ,6389 ,6397 ,6406 ,6626 ,6639 ,6809 ,6817 ,6844 ,6862 ,7127 ,7130 ,7151 ,7178 ,7193 ,7217 ,7246 ,7515 ,7572 ,7698 ,7716 ,7725 ,7737 ,7776 ,7855 ,7900 ,7909 ,7916 ,7974 ,7978 ,7983 ,8029 ,8055 ,8114 ,8128 ,8331 ,8358 ,8517 ,8632 ,8666 ,8710 ,8819 ,8854 ,8982 ,8999 ,9134 ,9230 ,9313 ,9348 ,9350 ,9400 ,9484 ,9515 ,9543 ,9785 ,9982]
+    bad_label = Label.objects.all().filter(line__in=Lines)
+    resulf = []
+    resulf = [{'line': l.line, 'label': l.result}
+                    for l in bad_label]
+    return JsonResponse({
+        'length' : len(resulf),
+        'result': resulf,
+        'status': True
+    }, safe=False, json_dumps_params={'ensure_ascii': False}, status =200)
+
 @api_view(['POST'])
 def update(request):
     data_body =json.loads(request.body.decode('utf-8'))
@@ -75,7 +89,7 @@ def runCommand(request):
     }, status=200)
 
 @api_view(['GET'])
-def runSelect3(request):
+def run1(request):
     bad_label = Label.objects.all().filter(is_good=0)
     print(len(bad_label))
     for label in bad_label:
